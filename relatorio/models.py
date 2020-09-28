c45bf2295ee2b8a975d87cf23d548badd4c8d421
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-27 23:23:06 -0300

    Criado os models do app relatorio.

--- relatorio/models.py
+++ relatorio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Consulta(models.Model):
+
+    numero_guia = models.IntegerField()
+    cod_medico = models.IntegerField()
+    nome_medico = models.CharField("Nome", max_length=100)
+    data_consulta = models.DateField()
+    valor_consulta = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Exame(models.Model):
+
+    numero_guia_consulta = models.ForeignKey(
+        Consulta, related_name="exames", on_delete=models.CASCADE
+    )
+    exame = models.CharField("Exame", max_length=50)
+    valor_exame = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return str(self.exame)
