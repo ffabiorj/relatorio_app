cd9a6dda73a9bfc8d8f1eb5b888cca34e9105d69
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-27 23:13:15 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,52 @@
+[![Build Status](https://travis-ci.com/ffabiorj/relatorio_app.svg?branch=master)](https://travis-ci.com/ffabiorj/voluntario_app)
+
+[![codecov](https://codecov.io/gh/ffabiorj/relatorio_app/branch/master/graph/badge.svg)](https://codecov.io/gh/ffabiorj/voluntario_app)
+
+# Criação de um app de relatório
+
+## Ferramentas
+
+- Django
+- Postgres
+
+## Roda o projeto localmente:
+
+1. Clone o repositório.
+2. Entre na pasta.
+3. Crie uma um ambiente de desenvolvimento com python 3.8.
+4. Ative o ambiente.
+5. Instale as dependências.
+6. Crie um arquivo .env
+7. Rode as migrações
+8. Rode o script para popular o BD
+9. Rode o projeto
+10. Acesse o link
+
+```
+- git clone git@github.com:ffabiorj/relatorio_app.git
+- cd relatorio_app
+- python3 -m venv .venv
+- source .venv/bin/activate
+- pip install -r requirements-dev.txt
+- python contrib/env_gen.py
+- python manage.py migrate
+- python import_data.py
+- python manage.py runserver
+- http://127.0.0.1:8000/
+```
+
+### Rodar os testes
+
+```
+pytest
+```
+
+### Links para as ferramentas utilizadas
+
+[Django](https://docs.djangoproject.com/)
+
+[Postgres](https://www.postgresql.org/)
+
+[Codecov](https://codecov.io/)
+
+[Travis](https://travis-ci.com/)
---
+++ import_data.py
@@ -0,0 +1,43 @@
+# from core.models import Consulta, Exame
+import csv
+import django
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "relatorio_app.settings")
+django.setup()
+
+from core.models import Consulta, Exame
+
+
+Consulta.objects.all().delete()
+Exame.objects.all().delete()
+
+consultas = []
+with open("contrib/consulta.csv") as file_csv:
+    consulta_csv = csv.DictReader(file_csv, delimiter=";")
+    for row in consulta_csv:
+        consulta = Consulta(
+            numero_guia=row["numero_guia"],
+            cod_medico=row["cod_medico"],
+            nome_medico=row["nome_medico"],
+            data_consulta=row["data_consulta"],
+            valor_consulta=row["valor_consulta"],
+        )
+        consultas.append(consulta)
+
+Consulta.objects.bulk_create(consultas)
+
+
+exames = []
+with open("contrib/exames.csv") as file_csv:
+    exame_csv = csv.DictReader(file_csv, delimiter=";")
+    for row in exame_csv:
+        numero_consulta = Consulta.objects.get(numero_guia=row["numero_guia_consulta"])
+        exame = Exame(
+            numero_guia_consulta=numero_consulta,
+            exame=row["exame"],
+            valor_exame=row["valor_exame"],
+        )
+        exames.append(exame)
+
+Exame.objects.bulk_create(exames)
